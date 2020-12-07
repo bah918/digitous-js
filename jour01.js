78adafbaa85a291486971c856bb12abc7f48b835
pknowledge <nafiou6@yahoo.fr>
2020-12-07 15:26:07 +0100

    exercie1-js

---
+++ jour01.js
@@ -0,0 +1,21 @@
+//01-HELLO WORLD
+console.log("hello world!");
+//02- String
+var test = "My name is BAH";
+console.log(test);
+//03 Concatenation
+var name= "BAH";
+console.log("Nice to meet you " + name);
+//04 String Length
+var testLength = " I'm very long !";
+console.log(testLength.length);
+//05 Replace
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good"));
+//06-Up and Down
+var basic = "This is cool";
+var basicUp = basic.toUpperCase();
+console.log(basicUp);
+basic=basicUp;
+var basicDown = basic.toLowerCase();
+console.log(basicUp);
