c10e3f288ddffb6383073b0611a19eae6e002073
pknowledge <nafiou6@yahoo.fr>
2020-12-08 14:41:14 +0100

    jour02

---
+++ jour02.js
@@ -0,0 +1,14 @@
+// O1 Number
+var integer = 102;
+var float = 13.9;
+console.log(`integer est : ${integer} float est :  ${float}`);
+console.log("Les deux valeurs est", integer,"&&", float);
+// 02 Convert
+var basic = 34;
+var stringified =basic.toString();
+// "xx".toString converti un nb en chaine de caractere
+console.log(stringified); 
+// 03 Round
+var num = 1.5
+var rounded = Math.round(num);
+console.log(rounded);
