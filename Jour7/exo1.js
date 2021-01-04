610d13a68558ccfd01480327b902651126a7e07d
pknowledge <nafiou6@yahoo.fr>
2021-01-04 23:25:06 +0100

    03-Map-Names

--- Jour7/exo1.js
+++ Jour7/exo1.js
@@ -7,12 +7,36 @@ fs.readFile("./jour7.txt", function (err, data) {
     
  });
  //02 Map double
-  
+ var array = [1,2,3,4,5];
  function mapDouble(array){
      return array.map(function(element){
          return element*2;
      })
  }
- var array = [1,2,3,4,5];
- console.log(mapDouble(array));      
+ 
+ console.log(mapDouble(array)); 
+ 
+ //03 Map-Names
+ var longNames=[
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+]
+ 
+var shortNames = longNames.map(function(choix){
+	 if(choix.firstName && choix.lastName){
+		return (`{name :"${choix.firstName + choix.lastName}"}`);
+	 }
+
+})
+
+
+console.log(shortNames);
+
+
 
